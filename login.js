7709391b01367c706e1c1a75ee1f1a1db9448eba
Jaipulant <jairopulidoa@gmail.com>
2026-09-18 14:39:10 +0200

    Conectar JS con la API en la nube

--- login.js
+++ login.js
@@ -10,7 +10,7 @@ document.getElementById('loginForm').addEventListener('submit', async function(e
     mensajeError.textContent = 'Usuario o contraseña incorrectos.'; // Reiniciar texto
 
     try {
-        const response = await fetch('http://localhost:8080/api/auth/login', {
+        const response = await fetch('https://datagrada.onrender.com/api/auth/login', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
